866a932367461b9402a3d658d211b38de722b67c
Brody <brodymccreath@gmail.com>
2019-05-03 10:26:08 +1200

    Created and started the questions and answers program

---
+++ Questions_and_Answers/Questions_and_Answers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Questions_and_Answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] answers = new string[10];
+            string[] questions = { "", ""};
+            
+            for(int i = 0; i < answers.Length; i++)
+            {
+                Console.Write(questions[i]);
+                answers[i] = Console.ReadLine();
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
