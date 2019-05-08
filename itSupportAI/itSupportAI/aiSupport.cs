71f108842e4a6ca86832419e93a22a4b053453ea
unknown <anthonymellon@yahoo.com>
2019-05-08 12:34:41 +1200

    Adding the proper code file

---
+++ itSupportAI/itSupportAI/aiSupport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itSupportAI
+{
+    class aiSupport
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, I am an IT support AI");
+            Console.ReadLine();
+        }
+
+    }
+}
